9b334a18c9e24733138d0f823f267d18bc2385ba
Max van den Hoven <max.hoven@gmail.com>
2023-05-11 22:22:45 +0200

    Finish exercise 1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 ##########################################################################################
 # Imports
 ##########################################################################################
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
 from deap import tools
 
 from src.data import load_dataset
@@ -45,16 +49,24 @@ outputs_1 = run_petrinas_ga(
         {"function": tools.cxTwoPoint},
     ],
     mutation_ops=[
-        {"function": tools.mutFlipBit, "indpb": 0.05},
+        {"function": tools.mutFlipBit, "indpb": 0.1},
     ],
     selection_ops=[
         {"function": tools.selTournament, "tournsize": 5},
     ],
     crossover_probs=[0.5],
-    mutation_probs=[0.2],
-    n_individuals=100,
-    n_generations=100,
-    n_iterations=3,
+    mutation_probs=[0.5],
+    n_individuals=500,
+    n_generations=50,
+    n_iterations=1,
 )
 
-# TODO: plotting
+fig, ax = plt.subplots(dpi=250, figsize=(8, 5))
+avg_fitness = outputs_1[0]["logbook"].select("avg")
+best_fitness = outputs_1[0]["logbook"].select("max")
+ax.plot(avg_fitness, label="Average fitness")
+ax.plot(best_fitness, label="Best fitness")
+ax.set_title("Fitness at each generation (1 run)")
+ax.set_xlabel("Generation")
+ax.legend()
+fig.savefig("plots/exercise-1-fitness.png")
